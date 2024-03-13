eacecbdb064ddb66d196daeb47a3bbd02bd7b06f
Cristopher <135583931+cristopher-namchee@users.noreply.github.com>
2024-03-13 10:29:42 +0700

    feat: update receipt and invoice contract with new contracts (#29)

--- ocr/invoice.go
+++ ocr/invoice.go
@@ -4,26 +4,34 @@ package ocr
 type Invoice = OCRResult[InvoiceData]
 
 type InvoiceData struct {
-	InvoiceNumber              OCRStringField `json:"invoice_number,omitempty"`
-	InvoiceDate                OCRStringField `json:"invoice_date,omitempty"`
-	InvoiceDueDate             OCRStringField `json:"invoice_due_date,omitempty"`
-	PurchaseOrder              OCRStringField `json:"purchase_order,omitempty"`
-	VendorName                 OCRStringField `json:"vendor_name,omitempty"`
-	VendorTaxId                OCRStringField `json:"vendor_tax_id,omitempty"`
-	VendorAddress              OCRStringField `json:"vendor_address,omitempty"`
-	CustomerName               OCRStringField `json:"customer_name,omitempty"`
-	CustomerTaxId              OCRStringField `json:"customer_tax_id,omitempty"`
-	CustomerAddress            OCRStringField `json:"customer_address,omitempty"`
-	ShippingAddress            OCRStringField `json:"shipping_address,omitempty"`
-	ShippingAddressRecipient   OCRStringField `json:"shipping_address_recipient,omitempty"`
-	PaymentTerm                OCRStringField `json:"payment_term,omitempty"`
-	SubTotalAmount             OCRIntField    `json:"sub_total_amount,omitempty"`
-	InvoiceTotalTax            OCRIntField    `json:"invoice_total_tax,omitempty"`
-	InvoiceTotal               OCRIntField    `json:"invoice_total,omitempty"`
-	RemittanceAddress          OCRStringField `json:"remittance_address,omitempty"`
-	RemittanceAddressRecipient OCRStringField `json:"remittance_address_recipient,omitempty"`
-	Currency                   OCRStringField `json:"currency,omitempty"`
-	Items                      []InvoiceItem  `json:"items,omitempty"`
+	InvoiceNumber       OCRStringField `json:"invoice_number,omitempty"`
+	InvoiceDate         OCRStringField `json:"invoice_date,omitempty"`
+	VATTax              OCRIntField    `json:"vat_tax,omitempty"`
+	IncomeTax           OCRIntField    `json:"income_tax,omitempty"`
+	InvoiceTotalTax     OCRIntField    `json:"invoice_total_tax,omitempty"`
+	InvoiceTotal        OCRIntField    `json:"invoice_total,omitempty"`
+	InvoiceSubTotal     OCRIntField    `json:"invoice_sub_total,omitempty"`
+	InvoiceDiscount     OCRIntField    `json:"invoice_discount,omitempty"`
+	DownPayment         OCRIntField    `json:"down_payment,omitempty"`
+	Voucher             OCRIntField    `json:"voucher,omitempty"`
+	VendorName          OCRStringField `json:"vendor_name,omitempty"`
+	VendorAddress       OCRStringField `json:"vendor_address,omitempty"`
+	VendorTaxId         OCRStringField `json:"vendor_tax_id,omitempty"`
+	CustomerName        OCRStringField `json:"customer_name,omitempty"`
+	CustomerAddress     OCRStringField `json:"customer_address,omitempty"`
+	CustomerTaxId       OCRStringField `json:"customer_tax_id,omitempty"`
+	SenderName          OCRStringField `json:"sender_name,omitempty"`
+	Currency            OCRStringField `json:"currency,omitempty"`
+	PurchaseOrderNumber OCRStringField `json:"purchase_order_number,omitempty"`
+
+	PaymentBanks []PaymentBank `json:"payment_banks,omitempty"`
+	Items        []InvoiceItem `json:"items,omitempty"`
+}
+
+type PaymentBank struct {
+	PaymentBankName      OCRStringField `json:"payment_bank_name,omitempty"`
+	PaymentAccountNumber OCRStringField `json:"payment_account_number,omitempty"`
+	PaymentAccountName   OCRStringField `json:"payment_account_name,omitempty"`
 }
 
 type InvoiceItem struct {
--- ocr/receipt.go
+++ ocr/receipt.go
@@ -4,20 +4,33 @@ package ocr
 type Receipt = OCRResult[ReceiptData]
 
 type ReceiptData struct {
-	MerchantName    OCRStringField `json:"merchant_name,omitempty"`
-	MerchantAddress OCRStringField `json:"merchant_address,omitempty"`
-	ReceiptDate     OCRStringField `json:"receipt_date,omitempty"`
-	ReceiptTime     OCRStringField `json:"receipt_time,omitempty"`
-	Items           []ReceiptItem  `json:"items,omitempty"`
-	SubTotalAmount  OCRStringField `json:"sub_total_amount,omitempty"`
-	TaxAmount       OCRIntField    `json:"tax_amount,omitempty"`
-	TipAmount       OCRIntField    `json:"tip_amount,omitempty"`
-	TotalAmount     OCRStringField `json:"total_amount,omitempty"`
+	MerchantName        OCRStringField `json:"merchant_name,omitempty"`
+	MerchantAddress     OCRStringField `json:"merchant_address,omitempty"`
+	MerchantPlace       OCRStringField `json:"merchant_place,omitempty"`
+	ReceiptDate         OCRStringField `json:"receipt_date,omitempty"`
+	ReceiptNumber       OCRStringField `json:"receipt_number,omitempty"`
+	ReceiptOrder        OCRStringField `json:"receipt_order,omitempty"`
+	ReceiptTime         OCRStringField `json:"receipt_time,omitempty"`
+	SubTotalAmount      OCRStringField `json:"sub_total_amount,omitempty"`
+	TaxAmount           OCRIntField    `json:"tax_amount,omitempty"`
+	DiscountAmount      OCRIntField    `json:"discount_amount,omitempty"`
+	ServiceChargeAmount OCRIntField    `json:"service_charge_amount,omitempty"`
+	TipAmount           OCRIntField    `json:"tip_amount,omitempty"`
+	TotalAmount         OCRStringField `json:"total_amount,omitempty"`
+	CCLast4Digit        OCRStringField `json:"cc_last_4_digit,omitempty"`
+	Currency            OCRStringField `json:"currency,omitempty"`
+	MerchantPhoneNumber OCRStringField `json:"merchant_phone_number,omitempty"`
+	PaymentMethod       OCRStringField `json:"payment_method,omitempty"`
+	PaymentProduct      OCRStringField `json:"payment_product,omitempty"`
+
+	Items []ReceiptItem `json:"items,omitempty"`
 }
 
 type ReceiptItem struct {
-	ItemName        OCRStringField `json:"item_name,omitempty"`
-	ItemQuantity    OCRStringField `json:"item_quantity,omitempty"`
-	ItemTotalPrice  OCRIntField    `json:"item_total_price,omitempty"`
-	ItemProductCode OCRStringField `json:"item_product_code,omitempty"`
+	ItemName         OCRStringField `json:"item_name,omitempty"`
+	ItemQuantity     OCRStringField `json:"item_quantity,omitempty"`
+	ItemTotalPrice   OCRIntField    `json:"item_total_price,omitempty"`
+	ItemProductCode  OCRStringField `json:"item_product_code,omitempty"`
+	ItemEachPrice    OCRIntField    `json:"item_each_price,omitempty"`
+	ItemQuantityUnit OCRStringField `json:"item_quantity_unit,omitempty"`
 }
