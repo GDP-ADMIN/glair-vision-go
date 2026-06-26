710f2ffcb60d23f33138afe224d2b5d4e0401fbd
CleveeJ <willsoncj285@gmail.com>
2026-06-26 13:52:39 +0700

    refactor(ocr): update KTPData fields to use OCRPageField for improved type consistency

--- ocr/ktp.go
+++ ocr/ktp.go
@@ -15,23 +15,23 @@ type KTPWithQuality struct {
 // KTPData stores key-value field of KTP that can be
 // recognized by the OCR engine
 type KTPData struct {
-	Agama            OCRField[string] `json:"agama,omitempty"`
-	Alamat           OCRField[string] `json:"alamat,omitempty"`
-	BerlakuHingga    OCRField[string] `json:"berlaku_hingga,omitempty"`
-	GolonganDarah    OCRField[string] `json:"golongan_darah,omitempty"`
-	JenisKelamin     OCRField[string] `json:"jenis_kelamin,omitempty"`
-	Kecamatan        OCRField[string] `json:"kecamatan,omitempty"`
-	KelurahanDesa    OCRField[string] `json:"kelurahan_desa,omitempty"`
-	Kewarganegaraan  OCRField[string] `json:"kewarganegaraan,omitempty"`
-	KotaKabupaten    OCRField[string] `json:"kota_kabupaten,omitempty"`
-	Nama             OCRField[string] `json:"nama,omitempty"`
-	Nik              OCRField[string] `json:"nik,omitempty"`
-	Pekerjaan        OCRField[string] `json:"pekerjaan,omitempty"`
-	Provinsi         OCRField[string] `json:"provinsi,omitempty"`
-	RtRw             OCRField[string] `json:"rt_rw,omitempty"`
-	StatusPerkawinan OCRField[string] `json:"status_perkawinan,omitempty"`
-	TanggalLahir     OCRField[string] `json:"tanggal_lahir,omitempty"`
-	TempatLahir      OCRField[string] `json:"tempat_lahir,omitempty"`
+	Agama            OCRPageField[string] `json:"agama,omitempty"`
+	Alamat           OCRPageField[string] `json:"alamat,omitempty"`
+	BerlakuHingga    OCRPageField[string] `json:"berlaku_hingga,omitempty"`
+	GolonganDarah    OCRPageField[string] `json:"golongan_darah,omitempty"`
+	JenisKelamin     OCRPageField[string] `json:"jenis_kelamin,omitempty"`
+	Kecamatan        OCRPageField[string] `json:"kecamatan,omitempty"`
+	KelurahanDesa    OCRPageField[string] `json:"kelurahan_desa,omitempty"`
+	Kewarganegaraan  OCRPageField[string] `json:"kewarganegaraan,omitempty"`
+	KotaKabupaten    OCRPageField[string] `json:"kota_kabupaten,omitempty"`
+	Nama             OCRPageField[string] `json:"nama,omitempty"`
+	Nik              OCRPageField[string] `json:"nik,omitempty"`
+	Pekerjaan        OCRPageField[string] `json:"pekerjaan,omitempty"`
+	Provinsi         OCRPageField[string] `json:"provinsi,omitempty"`
+	RtRw             OCRPageField[string] `json:"rt_rw,omitempty"`
+	StatusPerkawinan OCRPageField[string] `json:"status_perkawinan,omitempty"`
+	TanggalLahir     OCRPageField[string] `json:"tanggal_lahir,omitempty"`
+	TempatLahir      OCRPageField[string] `json:"tempat_lahir,omitempty"`
 	Foto             PhotoField       `json:"foto,omitempty"`
 	TandaTangan      PhotoField       `json:"tanda_tangan,omitempty"`
 }
